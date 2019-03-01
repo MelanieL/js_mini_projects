4b4a4d0ad9081fbb3820b96b4ba2a5aa13353d09
MelanieL <melanieleeking@gmail.com>
2019-02-28 20:20:38 -0500

    Setup HTML & basic CSS.

---
+++ 01_mad_libs/scripts/main.js
@@ -0,0 +1,8 @@
+// The plan:
+
+// 1 - Prompt the user for a noun
+// 2 - Prompt the user for an adjective
+// 3 - Prompt the user for an adverb
+// 4 - Take the answers and store them
+// 5 - Display mad lib with those words inserting into the blank spaces.
+
