7fe31c48e0114a7690cf84f535a794a3d9a29258
MelanieL <melanieleeking@gmail.com>
2019-03-05 19:28:44 -0500

    Fixed JS to force form validation for empty fields.

--- 01_mad_libs/scripts/main.js
+++ 01_mad_libs/scripts/main.js
@@ -9,12 +9,17 @@
 const submit = document.querySelector('.submit');
 const madLib = document.querySelector('.madLibBox');
 
-submit.addEventListener('click', displayMadLib);
+submit.addEventListener('click', displayAnswer);
 
-function displayMadLib() {
+function displayAnswer() {
     const noun = document.getElementById('noun').value;
     const adjective = document.getElementById('adjective').value;
     const adverb = document.getElementById('adverb').value;
+    console.log(noun, adjective, adverb);
+    if (noun == "" || adjective == "" || adverb == "") {
+    alert ("Oops! You must have missed a field!")        
+    } else {
     const finalMadLib = `<p class="madLibText">After they did that, the king played chess on his brother's <span class="userAnswer">${noun}</span> and then combed his <span class="userAnswer">${adjective}</span> hair with a comb made out of old fish bones. Later, that same day, I saw the Monkey King dance <span class="userAnswer">${adverb}</span> in front of an audience of kangaroos and wombats.</p>`
     madLib.innerHTML = finalMadLib;
+    }
 };
