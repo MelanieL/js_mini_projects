5ace4eca815123c320b59e8ab84321a9d68e9206
MelanieL <melanieleeking@gmail.com>
2019-03-26 20:15:37 -0400

    A js problem posted in CC Slack.

---
+++ 02_codercamp_puzzle/main.js
@@ -0,0 +1,11 @@
+// You have an array of strings but some strings in that array are comma separated. Goal is to go through the array and break out the comma separated strings into their own elements of the main array and the original string (the one with the commas) is removed
+// ['apple', 'orange', 'grape, pineapple'] turns into ['apple', 'orange', 'grape', 'pineapple']
+
+const initialArray = ['apple', 'orange', 'grape, pineapple'];
+const finalArray = initialArray.join(", ").split(", ");
+console.log(finalArray);
+
+
+
+
+
