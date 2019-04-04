0263c46c9b5fe2f7803e2f648f94fec6d25bcb41
MelanieL <melanieleeking@gmail.com>
2019-04-04 09:38:25 -0400

    Added folder with example from Medium article about targetting classes and IDs with javascript.

---
+++ 04_mediumarticleexample_classes_and_id_selection/main.js
@@ -0,0 +1,10 @@
+
+const vanillaDescription = document.querySelector('.vanillaCupcake');
+
+const chocolateDescription = document.getElementById('chocolateCupcake');
+
+function test() {
+    console.log("It worked!");
+}
+
+vanillaDescription.addEventListener('mouseover', test);
