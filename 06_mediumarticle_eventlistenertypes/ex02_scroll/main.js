df26254404b99cf0c40780c0485440a48a8a0031
MelanieL <melanieleeking@gmail.com>
2019-04-15 13:41:18 -0400

    Add scroll example and seperate examples

---
+++ 06_mediumarticle_eventlistenertypes/ex02_scroll/main.js
@@ -0,0 +1,17 @@
+// Turning my HTML cube div into a JavaScript variable so I can work with it
+const theCube = document.querySelector('.cube');
+const htmlClasses = document.querySelector('.cube').classList;
+console.log(htmlClasses);
+
+function toggleColour() {
+    if (htmlClasses.contains("cubePrimaryColour")) {
+        htmlClasses.remove('cubePrimaryColour');
+        htmlClasses.add('cubeSecondaryColour');
+    }
+    else {
+        htmlClasses.remove('cubeSecondaryColour');
+        htmlClasses.add('cubePrimaryColour');
+    }
+};
+
+document.addEventListener('scroll', toggleColour);
